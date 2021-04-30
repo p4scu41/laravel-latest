ec5a2a09604eb1a62e982b7fde698f68afd9dbaf
Pascual <pascual@genuitec.com>
2021-04-30 15:17:45 -0500

    Update to Laravel 8.40 & Jetstream 2.3

--- config/logging.php
+++ config/logging.php
@@ -76,6 +76,7 @@ return [
 
         'stderr' => [
             'driver' => 'monolog',
+            'level' => env('LOG_LEVEL', 'debug'),
             'handler' => StreamHandler::class,
             'formatter' => env('LOG_STDERR_FORMATTER'),
             'with' => [
